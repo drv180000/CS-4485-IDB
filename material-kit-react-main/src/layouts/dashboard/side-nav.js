1715bbde21870825ea395bc447c0d72008d157cc
Aaron Dang <94891631+wolf4500@users.noreply.github.com>
2023-03-31 10:27:59 -0500

    Change 'Devias Production' to 'UTD Discussion Board'

--- material-kit-react-main/src/layouts/dashboard/side-nav.js
+++ material-kit-react-main/src/layouts/dashboard/side-nav.js
@@ -71,13 +71,13 @@ export const SideNav = (props) => {
                 color="inherit"
                 variant="subtitle1"
               >
-                Devias
+                UTD 
               </Typography>
               <Typography
                 color="neutral.400"
                 variant="body2"
               >
-                Production
+                Discussion Board
               </Typography>
             </div>
             <SvgIcon
